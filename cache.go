0592c00e2a63658c7bb6377e98fb465a02d6eedd
HT4w5 <ht4w5x@gmail.com>
2026-04-02 14:35:11 +0800

    docs(cache): add comments

--- cache.go
+++ cache.go
@@ -1,7 +1,15 @@
 package cache
 
-import "github.com/zeebo/xxh3"
+import (
+	"github.com/zeebo/xxh3"
+)
 
+// Cache is a thread-safe, memory KV cache similar to
+// https://github.com/VictoriaMetrics/fastcache, created
+// to be self-educational. Use fastcache instead if aiming
+// for production use.
+//
+// Key and value total length must not exceed (64KB - 4B).
 type Cache struct {
 	rings []ring
 
@@ -11,6 +19,7 @@ type Cache struct {
 	numRings int
 }
 
+// New() creates a new cache.
 func New(opts ...func(*Cache)) *Cache {
 	c := &Cache{
 		numRings: 512,
@@ -22,7 +31,10 @@ func New(opts ...func(*Cache)) *Cache {
 	}
 
 	if c.numRings <= 0 {
-		panic("numRings must be positive")
+		c.numRings = 1
+	}
+	if c.size <= 0 {
+		c.size = 1
 	}
 
 	numShardsPerRing := (c.size + shardSize*uint64(c.numRings) - 1) / (shardSize * uint64(c.numRings))
@@ -40,12 +52,18 @@ func New(opts ...func(*Cache)) *Cache {
 
 // New() options
 
+// WithSize() sets max cache size in bytes.
+// Entries will be evicted when cache size exceeds max cache size.
+// Default cache size is 64KiB * NumRings if not set or lower is set.
 func WithSize(size uint64) func(*Cache) {
 	return func(c *Cache) {
 		c.size = size
 	}
 }
 
+// WithNumRings() sets number of rings used in cache.
+// Defaults to 512.
+// Sets to 1 if lower is set.
 func WithNumRings(numRings int) func(*Cache) {
 	return func(c *Cache) {
 		c.numRings = numRings
@@ -54,12 +72,19 @@ func WithNumRings(numRings int) func(*Cache) {
 
 // Methods
 
+// Set() sets k, v into the cache.
+// KV pairs with total length exceeding (64KB - 4B) are silently dropped.
+//
+// k and v are safe to be modified after Set() returns
 func (c *Cache) Set(k, v []byte) {
 	h := xxh3.Hash(k)
 	ringIdx := h % uint64(c.numRings)
 	c.rings[ringIdx].set(k, v, h)
 }
 
+// Get() copies value for key k into dst[:len(value)].
+// A new slice will be allocated if dst is nil or not long enough.
+// Returns nil if not found.
 func (c *Cache) Get(dst, k []byte) []byte {
 	h := xxh3.Hash(k)
 	ringIdx := h % uint64(c.numRings)
@@ -67,12 +92,20 @@ func (c *Cache) Get(dst, k []byte) []byte {
 	return dst
 }
 
+// HasGet() copies value for key k into dst[:len(value)] if pair exists.
+// A new slice will be allocated if dst is nil or not long enough.
+// Returns nil, false if not found.
+//
+// HasGet() is equal to Get() in performance.
 func (c *Cache) HasGet(dst, k []byte) ([]byte, bool) {
 	h := xxh3.Hash(k)
 	ringIdx := h % uint64(c.numRings)
 	return c.rings[ringIdx].get(dst, k, h, true)
 }
 
+// Has() checks whether key k exists in cache.
+//
+// Has() is slightly cheaper than HasGet() and Get().
 func (c *Cache) Has(k []byte) bool {
 	h := xxh3.Hash(k)
 	ringIdx := h % uint64(c.numRings)
@@ -80,12 +113,14 @@ func (c *Cache) Has(k []byte) bool {
 	return ok
 }
 
+// Del() deletes KV pair from cache with key k.
 func (c *Cache) Del(k []byte) {
 	h := xxh3.Hash(k)
 	ringIdx := h % uint64(c.numRings)
 	c.rings[ringIdx].del(h)
 }
 
+// Reset removes all KV pairs from cache
 func (c *Cache) Reset() {
 	for i := range c.numRings {
 		c.rings[i].reset()
